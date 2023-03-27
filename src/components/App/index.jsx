8c580cdc0f660284d0c7c936c02a4bb480dd3229
Jefferson <radsjefferson@gmail.com>
2023-03-26 19:44:10 -0600

    💄 [ADD] Footer

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,6 +2,7 @@ import { useState } from 'preact/hooks'
 import { createContext } from 'preact'
 import { Main } from '../Main'
 import { Sidebar } from '../Sidebar'
+import { Footer } from '../Footer'
 import './app.scss'
 
 export const currentArticleContext = createContext()
@@ -14,6 +15,7 @@ export function App() {
       <currentArticleContext.Provider value={{currentArticle, setCurrentArticle}}>
         <Main />
         <Sidebar />
+        <Footer />
       </currentArticleContext.Provider>
     </>
   )
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import './footer.scss'
+
+export function Footer () {
+  return (
+    <div className="footer">
+      <p>Hecho por <a target="_blank" rel="noreferrer noopener" href="https://github.com/Jefferson-R">Jefferson Stanley Ramos Damas</a></p>
+    </div>
+  )
+}
